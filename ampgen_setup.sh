23a7b97ee7857fbf0adbe976251b59c423e249cc
Jake Lane <john.jake.lane@cern.ch>
2020-03-06 18:05:11 +0000

    typo in ampgen_setup.sh

--- ampgen_setup.sh
+++ ampgen_setup.sh
@@ -1,4 +1,4 @@
 source /sw/root/bin/thisroot.sh
 export LD_LIBRARY_PATH=/sw/AmpGen/install/lib/:$LD_LIBRARY_PATH
-export AMPGENROOT=/sw/AmpGen
+export AMPGENROOT=/sw/AmpGen/
 export PATH=$AMPGENROOT/install/bin:$PATH
